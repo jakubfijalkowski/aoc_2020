91a7109e616b1e84972e815dd2a1557f0c4c2406
Jakub Fijałkowski <kuba@codinginfinity.me>
2020-12-02 00:27:24 +0100

    Simplify `Pairwise` and make it behave correctly in case of empty collections

--- src/pairwise.rs
+++ src/pairwise.rs
@@ -1,13 +1,16 @@
+use std::iter::FusedIterator;
+
 pub struct Pairwise<I1, I2>
 where
-    I1: IntoIterator<Item: Copy>,
-    I2: IntoIterator + Copy,
+    I1: IntoIterator,
+    I2: IntoIterator,
 {
     iter2: I2,
 
-    curr_item1: I1::Item,
     curr_iter1: I1::IntoIter,
     curr_iter2: I2::IntoIter,
+
+    curr_item1: Option<I1::Item>,
 }
 
 impl<I1, I2> Iterator for Pairwise<I1, I2>
@@ -18,18 +21,28 @@ where
     type Item = (I1::Item, I2::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(y) = self.curr_iter2.next() {
-            Some((self.curr_item1, y))
-        } else {
+        if self.curr_item1.is_none() {
             if let Some(x) = self.curr_iter1.next() {
-                self.curr_iter2 = self.iter2.into_iter();
-                self.curr_item1 = x;
+                self.curr_item1 = Some(x);
                 self.next()
             } else {
                 None
             }
+        } else if let Some(y) = self.curr_iter2.next() {
+            Some((self.curr_item1.unwrap(), y))
+        } else {
+            self.curr_item1 = self.curr_iter1.next();
+            self.curr_iter2 = self.iter2.into_iter();
+            self.next()
         }
     }
+}
+
+impl<I1, I2> FusedIterator for Pairwise<I1, I2>
+where
+    I1: IntoIterator<Item: Copy, IntoIter: FusedIterator>,
+    I2: IntoIterator + Copy,
+{
 }
 
 impl<I1, I2> Pairwise<I1, I2>
@@ -38,14 +51,13 @@ where
     I2: IntoIterator + Copy,
 {
     pub fn from(a: I1, b: I2) -> Self {
-        let mut i = a.into_iter();
         Pairwise {
             iter2: b,
 
-            curr_item1: i.next().unwrap(),
-
-            curr_iter1: i,
+            curr_iter1: a.into_iter(),
             curr_iter2: b.into_iter(),
+
+            curr_item1: None,
         }
     }
 }
@@ -55,10 +67,35 @@ mod tests {
     use super::*;
 
     #[test]
+    fn empty() {
+        let empty: [i32; 0] = [];
+        let a = [1];
+
+        let mut p = Pairwise::from(&a, &empty);
+        assert_eq!(None, p.next());
+
+        let mut p = Pairwise::from(&empty, &a);
+        assert_eq!(None, p.next());
+
+        let mut p = Pairwise::from(&empty, &empty);
+        assert_eq!(None, p.next());
+    }
+
+    #[test]
     fn single_element() {
         let a = [1];
         let mut p = Pairwise::from(&a, &a);
         assert_eq!(Some((&1, &1)), p.next());
+        assert_eq!(None, p.next());
+    }
+
+    #[test]
+    fn restartable() {
+        let a = [1];
+        let mut p = Pairwise::from(&a, &a);
+        assert_eq!(Some((&1, &1)), p.next());
+        assert_eq!(None, p.next());
+        assert_eq!(None, p.next());
         assert_eq!(None, p.next());
     }
 
