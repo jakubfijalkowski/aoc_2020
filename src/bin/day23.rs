0422cb00333fbe33b096f290f5c24174fde4903a
Jakub Fijałkowski <kuba@codinginfinity.me>
2020-12-24 20:49:40 +0100

    Solve day 23 the correct way
    With index-based linked list

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,127 +1,107 @@
 #![feature(assoc_char_funcs)]
 
 #[derive(Debug)]
-struct RingBuffer<T>(Vec<T>);
+struct RingBuffer(Vec<(usize, usize)>);
 
-impl<T> RingBuffer<T> {
-    fn new(v: Vec<T>) -> Self {
-        RingBuffer(v)
+impl RingBuffer {
+    fn new(max_value: usize) -> Self {
+        let mut result: Vec<_> = (0..=max_value)
+            .map(|i| (i.saturating_sub(1), i + 1))
+            .collect();
+        result[0] = (usize::MAX, usize::MAX); // Sentinel
+        result[1].0 = max_value;
+        result[max_value].1 = 1;
+        RingBuffer(result)
     }
 
-    fn get(&self, idx: usize) -> &T {
-        let idx = idx % self.0.len();
-        &self.0[idx]
-    }
-
-    fn remove_after(&mut self, idx: usize) -> [T; 3]
-    where
-        T: Copy,
-    {
-        let idx1 = self.coerce(idx + 1);
-        let idx2 = self.coerce(idx + 2);
-        let idx3 = self.coerce(idx + 3);
-        let res = [self.0[idx1], self.0[idx2], self.0[idx3]];
-        if idx3 > idx1 {
-            self.0.drain(idx1..=idx3);
-        } else if idx2 > idx1 {
-            self.0.remove(idx2);
-            self.0.remove(idx1);
-            self.0.remove(idx3);
-        } else {
-            self.0.remove(idx1);
-            self.0.remove(idx3);
-            self.0.remove(idx2);
+    fn reorder(&mut self, input: &[usize]) {
+        for (idx, v) in input.iter().enumerate() {
+            let prev = input[if idx == 0 { input.len() - 1 } else { idx - 1 }];
+            let next = input[if idx == input.len() - 1 { 0 } else { idx + 1 }];
+            self.0[*v] = (prev, next);
+            self.0[prev].1 = *v;
+            self.0[next].0 = *v;
         }
-        res
+
+        if input.len() + 1 < self.0.len() {
+            let last_idx = self.0.len() - 1;
+            self.0[input[0]].0 = self.0.len() - 1;
+            self.0[last_idx].1 = input[0];
+
+            self.0[*input.last().unwrap()].1 = input.len() + 1;
+            self.0[input.len() + 1].0 = *input.last().unwrap();
+        }
     }
 
-    fn insert_after(&mut self, idx: usize, data: [T; 3])
-    where
-        T: Copy,
-    {
-        let idx = idx + 1;
-        self.0.splice(idx..idx, data.iter().copied());
+    fn prev(&self, v: usize) -> usize {
+        self.0[v].0
     }
 
-    fn find_index(&self, v: &T, start_at: usize) -> Option<usize>
-    where
-        T: PartialEq,
-    {
-        for i in 0..self.0.len() {
-            if self.get(start_at + i).eq(v) {
-                return Some(start_at + i);
+    fn next(&self, v: usize) -> usize {
+        self.0[v].1
+    }
+
+    fn turn(&mut self, v: usize) -> usize {
+        let f1 = self.next(v);
+        let f2 = self.next(f1);
+        let f3 = self.next(f2);
+
+        let f4 = self.next(f3);
+        self.0[v].1 = f4;
+        self.0[f4].0 = v;
+
+        let spliced = [f1, f2, f3];
+        let mut dst = v - 1;
+        while dst == 0 || spliced.contains(&dst) {
+            if dst == 0 {
+                dst = self.0.len() - 1;
+            } else {
+                dst -= 1;
             }
         }
-        None
+
+        let prev_next = self.0[dst].1;
+        self.0[dst].1 = f1;
+        self.0[f1] = (dst, f2);
+        self.0[f2] = (f1, f3);
+        self.0[f3] = (f2, prev_next);
+        self.0[prev_next].0 = f3;
+
+        self.next(v)
     }
-
-    fn coerce(&self, idx: usize) -> usize {
-        idx % self.0.len()
-    }
-}
-
-fn find_dest(buffer: &RingBuffer<u32>, mut cnt: u32, removed: &[u32; 3], max: u32) -> usize {
-    cnt -= 1;
-    while cnt == 0 || removed.contains(&cnt) {
-        if cnt == 0 {
-            cnt = max;
-        } else {
-            cnt -= 1;
-        }
-    }
-    buffer.find_index(&cnt, 0).unwrap()
-}
-
-fn turn(buffer: &mut RingBuffer<u32>, idx: usize, max: u32) -> usize {
-    let cnt = *buffer.get(idx);
-    let nums = buffer.remove_after(idx);
-
-    let dst_idx = find_dest(buffer, cnt, &nums, max);
-    buffer.insert_after(dst_idx, nums);
-    buffer.coerce(buffer.find_index(&cnt, idx - 3).unwrap() + 1)
 }
 
 fn part1() {
     const MOVES: usize = 100;
-    let buffer: Vec<u32> = include_str!("../../inputs/day23.txt")
+    let initial_order: Vec<_> = include_str!("../../inputs/day23.txt")
         .chars()
         .filter(|c| c.is_digit(10))
-        .map(|x| x as u32 - '0' as u32)
+        .map(|x| x as usize - '0' as usize)
         .collect();
-    let mut buffer = RingBuffer::new(buffer);
-    (0..MOVES).fold(0, |idx, _| turn(&mut buffer, idx, 9));
-    let one = buffer.find_index(&1, 0).unwrap();
-    let result = (1..9).fold(String::new(), |mut s, i| {
-        s.push(char::from_u32(buffer.get(one + i) + '0' as u32).unwrap());
-        s
+    let mut buffer = RingBuffer::new(9);
+    buffer.reorder(&initial_order);
+    (0..MOVES).fold(initial_order[0], |v, _| buffer.turn(v));
+    let (result, _) = (1..9).fold((String::new(), buffer.next(1)), |(s, v), _| {
+        (format!("{}{}", s, v), buffer.next(v))
     });
 
     println!("Part 1: {}", result);
 }
 
 fn part2() {
-    const MAX_VALUE: u32 = 1000000;
+    const MAX_VALUE: usize = 1000000;
     const MOVES: usize = 10000000;
-    let mut buffer: Vec<u32> = include_str!("../../inputs/day23.txt")
+    let initial_order: Vec<_> = include_str!("../../inputs/day23.txt")
         .chars()
         .filter(|c| c.is_digit(10))
-        .map(|x| x as u32 - '0' as u32)
+        .map(|x| x as usize - '0' as usize)
         .collect();
-    let max = buffer.iter().max().unwrap();
-    for i in max + 1..=MAX_VALUE {
-        buffer.push(i);
-    }
-
-    let mut buffer = RingBuffer::new(buffer);
-    (0..MOVES).fold(0, |idx, i| {
-        if i % 100000 == 0 {
-            println!("Iter: {}", i);
-        }
-        turn(&mut buffer, idx, MAX_VALUE)
-    });
-    let one = buffer.find_index(&1, 0).unwrap();
-    let result = (*buffer.get(one + 1) as u64) * (*buffer.get(one + 1) as u64);
-    println!("Part 2: {}", result);
+    let mut buffer = RingBuffer::new(MAX_VALUE);
+    buffer.reorder(&initial_order);
+    (0..MOVES).fold(initial_order[0], |v, _| buffer.turn(v));
+    let a = buffer.next(1);
+    let b = buffer.next(a);
+    println!("Part 2: {}", a * b);
 }
 
 fn main() {
